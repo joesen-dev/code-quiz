cf6dbe4e1e5b8f20c51578dd3469b1723c66e03d
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-06-20 17:35:01 -0400

    basic css

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var questions = [
+    {
+        question: "If you type the following code in the console window, what result will you get?\n3 > 2 > 1 === false;",
+        answer: "True"        
+    }
+];
+var score = 0;
+
+for( var i = 0; i < questions.length; i++) {
+    
+}
