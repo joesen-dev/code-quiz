29eb38730540dfbd3f6265edd1bf0bbfab0f9f49
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-06-21 20:31:53 -0400

    questions & answers print to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,14 @@ answerIdCounter = 0;
 
 var mainEl = document.querySelector("#page-content");
 var formEl = document.querySelector("#wrapper");
+var questionDivEl = document.querySelector("#question-div");
+var h1El = document.querySelector("#title");
+var pEl = document.querySelector("#description");
 var buttonDivEl = document.querySelector("#answers-div")
+var buttonEl = document.querySelector("#start");
 
 // STORE QUESTIONS with correct answer
-var questionsAry = [
+const questionsAry = [
     {
         question: "If you type the following code in the console window, what result will you get?\n3 > 2 > 1 === false;",
         answer: "True"                
@@ -21,66 +25,100 @@ var questionsAry = [
 ];
 
 // STORE ANSWERS
-var answersAry = [
-    {
-    // Question # 1    
-    q1Answer1: "True",                
-    q1Answer2: "False"
-    },
-    {
+const answersAry = [    
+    // Question # 1        
+    ["True", "False"],
     // Question # 2
-    q2Answer1: "Client",                
-    q2Answer2: "Server",                
-    q2Answer1: "Both",                
-    q2Answer2: "None",
-    },
-    {                
+    ["Client", "Server", "Both", "None"],             
     // Question # 3
-    q3Answer1: "Catch",                
-    q3Answer2: "Label",                
-    q3Answer1: "Try",                
-    q3Answer2: "Default"
-    }                
+    ["Catch", "Label", "Try", "Default"]             
 ];
+
+// Target answersAry rows to get answers list
+answersAry.forEach(function(getAnswer) {
+    console.log(getAnswer);
+    // target answersAry rows to print each answer
+    getAnswer.forEach(function(printAnswer) {
+        console.log(printAnswer);
+    });
+})
+
+// package answers
+const answerDataObj = {
+    a: [],
+    b: [],
+    c: [],
+    d: []
+}
+
+// set variables to taxi answers data
+var answerA = answerDataObj.a;
+var answerB = answerDataObj.b;
+var answerC = answerDataObj.c;
+var answerD = answerDataObj.d;
 
 var score = 0;
 
+// This block creates HTML elements and attached them to the form
+// TODO: can be used closer to the end of quiz IGNORE FOR NOW!
+var createQuestion = function() {
+    // debugger;
+    var questionDivEl = document.createElement("div");          
+    questionDivEl.setAttribute("id", "question-div");
+    console.log(questionDivEl); 
+    formEl.appendChild(questionDivEl);    
+    var h1El = document.createElement("h1");  
+    h1El.className = "question";
+    h1El.setAttribute("id", "title");
+    console.log(h1El);     
+    questionDivEl.appendChild(h1El);
+    console.log(questionDivEl);
+}
 
-var questionsGenerator = function() {
-    // formEl.innerHTML = "";
-    debugger; 
-        
-    for(var i = 0; i < questionsAry.length; i++) {     
-        // ON START QUIZ CLICK = replace form element
-        formEl.innerHTML = ""
-        buttonDivEl.innerHTML = ""
+
+// TODO: use this to list quiz answers
+var listAnswers = function() {
+    // debugger;    
+    // Target answersAry rows to get answers list
+    answersAry.forEach(function(getAnswer) {
+    // debugger
+        console.log(getAnswer);
+        // target answersAry rows to print each answer
+        getAnswer.forEach(function(printAnswer) {
+            console.log(printAnswer);
+            var buttonEl = document.createElement("button");
+            console.log(buttonEl);
+            buttonEl.className = "answers btn"    
+            buttonEl.setAttribute("id", answerIdCounter);        
+            buttonDivEl.appendChild(buttonEl);
+            console.log(buttonDivEl)
+            // debugger;
+            // TODO: filter answers for current questions before printing them to th button
+            
+            // TODO: have this list answers
+            buttonEl.innerHTML = printAnswer;
+        });
+    })
+}
+
+var questionsGenerator = function() {    
+    document.getElementById("title").innerHTML = ""
+    document.getElementById("description").innerHTML = ""
+    var startQuiz = document.getElementById("start");
+    startQuiz.remove();   
+    // createQuestion();    
+
+    for(var i = 0; i < questionsAry.length; i++) {      
+        // debugger;             
         var quizQuestion = questionsAry[i].question;
-        console.log(quizQuestion)
 
-        // This block creates a new question
-        var questionDivEl = document.createElement("div");   
-        questionDivEl.setAttribute("id", "question-div");
-        formEl.appendChild(questionDivEl);    
-        var h1El = document.createElement("h1");
-        h1El.className = "question";     
-        questionDivEl.appendChild(h1El);
-        formEl.innerHTML =
-            "<h1>" + questionsAry[i].question + "</h1>"
-    
-        //TODO: make this  create container for answers        
-        var answerButtonEl = document.createElement("button");
-        console.log(answerButtonEl);
-        answerButtonEl.className = "answers"    
-        answerButtonEl.setAttribute("id", answerIdCounter);
-        console.log(answerButtonEl);
-        buttonDivEl.appendChild(answerButtonEl);
-        console.log(buttonDivEl)
-        answerButtonEl.innerHTML =         
-            "<button class='answers'>" + answersAry[i] + "</button>" + 
-            "<button class='answers'>" + answersAry[i] + "</button>" 
-        console.log(answerButtonEl);
-             
-        answerIdCounter++;         
+        // TODO: may be a good place for a while loop to line up answers with questions
+
+        console.log(quizQuestion)
+        document.getElementById("title").innerHTML = questionsAry[i].question;
+        listAnswers();        
+            
+    answerIdCounter++;            
     }    
 }
 var quizFormHandler = function(event) {
@@ -89,10 +127,13 @@ var quizFormHandler = function(event) {
 
     // if clicked run a new function    
     questionsGenerator(); 
-    // createQuestionEl()
-    // debugger;
-  
+      
 }
-
 // Start Quiz
 mainEl.addEventListener("submit", quizFormHandler);
+
+
+
+// ___________________________________________________________________________
+
+
